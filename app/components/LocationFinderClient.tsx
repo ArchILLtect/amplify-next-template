2c51fdcdd082ebaa5011f9bd369191635f296a1a
ArchILLtect <nickhansonsr@gmail.com>
2025-11-23 20:01:11 -0600

    feat: enhance LocationFinderClient to include loading state and additional location data

--- app/components/LocationFinderClient.tsx
+++ app/components/LocationFinderClient.tsx
@@ -1,28 +1,46 @@
 'use client';
 
 import { useEffect, useState } from "react"
+import { Spinner } from "./Spinner";
+import Forcast from "./Forcast";
 
 export default function LocationFinderClient() {
-    const [locationInfo, setLocationInfo] = useState({city : "", state : ""});
+  const [loading, setLoading] = useState(true);
+  const [locationInfo, setLocationInfo] = useState({city : "", state : "", lat: "", long: ""});
 
-    const fetchLocationData = async () => {
-        // Example: Fetch location data from an API
-        const response = await fetch('https://apip.cc/json')
-        const data = await response.json();
-        const city = JSON.parse(JSON.stringify(data.City));
-        const state = JSON.parse(JSON.stringify(data.RegionName));
-        setLocationInfo({city: city, state: state});
-    }
+  const fetchLocationData = async () => {
+    const response = await fetch('https://apip.cc/json');
+    const data = await response.json();
 
-    useEffect(() => {
-        // Example: Fetch location data from an API
-        fetchLocationData();
-    }, [ ]);
+    const city = JSON.parse(JSON.stringify(data.City));
+    const state = JSON.parse(JSON.stringify(data.RegionName));
+    const lat = JSON.parse(JSON.stringify(data.Latitude));
+    const long = JSON.parse(JSON.stringify(data.Longitude));
+    setLocationInfo({ city, state, lat, long });
 
-    return (
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    // Example: Fetch location data from an API
+    fetchLocationData();
+
+  }, []);
+
+  return (
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', padding: '1rem', border: '1px solid gray', borderRadius: '1rem', backgroundColor: '#814bbf', color: '#faf9f7'}}>
+      <h2 style={{margin: 0, fontWeight: '500'}}>Client Component</h2>
+      <h3 style={{marginTop: '1rem', marginBottom: 0, fontWeight: '500'}}>Location:</h3>
+      {loading ? (
+        <div>
+          <Spinner />
+        </div>
+      ) : (
         <>
-            <h1>Location Finder - Client Component</h1>
-            <h2>{locationInfo.city}, {locationInfo.state}</h2>
+          <li style={{margin: '0 1rem 1rem 1rem', listStyleType: 'none', color: 'black'}}>{locationInfo.city}, {locationInfo.state}</li>
+          <Forcast lat={locationInfo.lat} long={locationInfo.long} />
         </>
-    )
+      )}
+    </div>
+  )
 }
