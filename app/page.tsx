13e788803aabd746f72bea75d99b570674739206
ArchILLtect <nickhansonsr@gmail.com>
2025-11-23 20:16:06 -0600

    style: enhance layout and typography for Location Information and todos sections

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,15 @@ export default function App() {
 
   return (
     <main style={{width: '90%'}}>
-      <div style={{ border: '1px solid black', borderRadius: '1rem', padding: '1rem', textAlign: 'center', marginBottom: '2rem' }}>
-        <h1>Location Information</h1>
+      <div style={{ border: '1px solid black', borderRadius: '1rem', paddingTop: '1rem', paddingBottom: '3rem', textAlign: 'center', marginBottom: '2rem' }}>
+        <h1 style={{fontWeight: '500', marginTop: 0, marginBottom: '1rem'}}>Location Information</h1>
         <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
           <LocationFinderClient />
           <LocationFinderServer />
         </div>
       </div>
-      <div>
-        <h1>ArchILLtect's todos</h1>
+      <div style={{ padding: '1rem', width: '80%', margin: '0 auto' }}>
+        <h1 style={{fontWeight: '500', marginTop: 0, marginBottom: '1rem'}}>ArchILLtect's todos</h1>
         <ToDo />
       </div>
     </main>
